3bc28bef2fa8883b767362d3be279987d8534b72
Raúl <rvelasquezvega@cifpfbmoll.eu>
2023-06-06 01:31:29 +0200

    añadir comentarios al java script

--- Frontend/js.js
+++ Frontend/js.js
@@ -1,53 +1,30 @@
-/*
-function enviarPOST() {
-    var http = new XMLHttpRequest();
-// Aqui pondremos donde queremos que se vaya enviando la informacion 
-// (ordenador localhost,port 3000,projecta tomcat,clase Login, true es que me deixa fer coses una vegada enviat, 
-// no necesit esperar a que me contesti)
-
-    let mail = document.getElementById("mail").value
-    let pass = document.getElementById("pass").value
-    http.open("POST", "http://localhost:3000/Tomcat/Login",true);
-    // peticio des atributs segons es tipus arxiu
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // si volem cercar es tipos de content-type aquest es per clau-valor
-    // enviam
-    http.send("mail="+mail+"&pass="+pass);
-
-    http.onreadystatechange = function() {
-        if(this.readyState==4 && this.status==200) {   // es numero 4 es es darrer pas de verificacio i 200 es que ha anat tot be
-            let session = this.responseText;
-            if(session!=0){
-                window.sessionStorage.setItem("mail",mail);
-                window.sessionStorage.setItem("session",session);
-                document.getElementById("resultat").innerHTML="Login correcto";
-            }else{
-                document.getElementById("resultat").innerHTML="Login incorrecto";
-            }
-        }
-    }
-}
-
-*/
+// Función para realizar una solicitud GET al servidor
 function enviarGET() {
     var http = new XMLHttpRequest();
     let mail = document.getElementById("mail").value;
     let pass = document.getElementById("pass").value;
+
+    // Abrir una solicitud GET al servidor para realizar el login
     http.open("GET", "http://localhost:3000/Entornos/Login?mail=" + mail + "&pass=" + pass, true);
     http.send();
+
     http.onreadystatechange = function() {
         if (http.readyState == 4 && http.status == 200) {
             let session = http.responseText;
-            if (session !="0") {
+            if (session != "0") {
+                // Si se recibe una sesión válida, guardar el correo y la sesión en sessionStorage
                 window.sessionStorage.setItem("mail", mail);
                 window.sessionStorage.setItem("session", session);
                 console.log("Respuesta del servidor: " + session); // Agrega este console.log para depurar
 
-                document.getElementById("resultat").innerHTML="Login correcto";
-                //pausa de 7 decimas
+                document.getElementById("resultat").innerHTML = "Login correcto";
+                
+                // Pausa de 7 décimas de segundo antes de redirigir a la página de gestión
                 setTimeout(function() {
                     gestion();
-                },700);
+                }, 700);
             } else {
+                // Si se recibe una sesión inválida, mostrar un mensaje de login incorrecto
                 document.getElementById("resultat").innerHTML = "Login incorrecto";
                 console.log("Respuesta del servidor: " + session); // Agrega este console.log para depurar
             }
@@ -55,68 +32,77 @@ function enviarGET() {
     }
 }
 
-function gestion(){
-    var codigoSesion =sessionStorage.getItem("session");
+// Función para redirigir a la página de gestión
+function gestion() {
+    var codigoSesion = sessionStorage.getItem("session");
     if (codigoSesion != 0) {
-        // Redirigir a la página de Gestión
+        // Si se ha iniciado sesión, redirigir a la página de gestión
         window.location.href = "Gestion.html";
     }
 }
 
-function logOut(){
-    //borra el correo  y la sesion del sessionStorage
+// Función para cerrar sesión
+function logOut() {
+    // Borrar el correo y la sesión del sessionStorage
     sessionStorage.removeItem('mail');
-    sessionStorage.removeItem('sesion');
-    //Redirige a la pagina de Login
-    window.location.href='Login.html';
-}
-function irAlta() {
+    sessionStorage.removeItem('session');
+    // Redirigir a la página de login
+    window.location.href = 'Login.html';
 
+}
+// Función para redirigir a la página de Alta o al Login
+function irAlta() {
     // Obtengo el valor del sessionStorage
     var codigoSesion = sessionStorage.getItem("session");
-    // Verifica si se ha guardado la información
+    
+    // Verifica si se ha guardado la información de la sesión
     if (codigoSesion != 0) {
-
-        // Redirigir a la página de Gestión
+        // Si se ha iniciado sesión, redirigir a la página de Alta
         window.location.href = "Alta.html";
     } else {
-
+        // Si no se ha iniciado sesión, redirigir a la página de Login
         window.location.href = "Login.html";
     }
 }
 
-function getTable(){
-    //Obtiene el correo electronico y la sesion del sessionStorage
+// Función para obtener la tabla de datos
+function getTable() {
+    // Obtener el correo electrónico y la sesión del sessionStorage
     var mail = sessionStorage.getItem('mail');
     var session = sessionStorage.getItem('session');
-    //envia una solicitud GET al backend con los parametros mail y session
+    
+    // Enviar una solicitud GET al backend con los parámetros mail y session
     var http = new XMLHttpRequest();
-    http.open('GET',"http://localhost:3000/Entornos/ServeXips?mail=" + mail + "&session=" +session, true);
+    http.open('GET', "http://localhost:3000/Entornos/ServeXips?mail=" + mail + "&session=" + session, true);
     http.send();
-    http.onreadystatechange = function(){
-        if(http.readyState==4 && http.status===200){
-             // Asigna la respuesta recibida a la variable responseHtml
+    
+    http.onreadystatechange = function() {
+        if (http.readyState == 4 && http.status === 200) {
+            // Asignar la respuesta recibida a la variable tabla
             let tabla = http.responseText;
             document.getElementById("tabla").innerHTML = tabla;
         }
     }
 }
 
-function getPatients(){
-        //Obtiene el correo electronico y la sesion del sessionStorage
+// Función para obtener la lista de pacientes
+function getPatients() {
+    // Obtener el correo electrónico y la sesión del sessionStorage
     var mail = sessionStorage.getItem('mail');
     var session = sessionStorage.getItem('session');
-        //envia una solicitud GET al backend con los parametros mail y session
+    
+    // Enviar una solicitud GET al backend con los parámetros mail y session
     var http = new XMLHttpRequest();
-    http.open('GET',"http://localhost:3000/Entornos/ServePatients?mail=" + mail + "&session=" +session, true);
+    http.open('GET', "http://localhost:3000/Entornos/ServePatients?mail=" + mail + "&session=" + session, true);
     http.send();
-    http.onreadystatechange = function () {
+    
+    http.onreadystatechange = function() {
         if (http.readyState == 4 && http.status == 200) {
             let listaPacientes = JSON.parse(http.responseText);
             
             let select = document.getElementById("seleccionPacientes");
-
-
+            
+            // Recorrer la lista de pacientes y crear opciones para el elemento select
             for (let i = 0; i < listaPacientes.length; i++) {
                 let option = document.createElement("option");
                 option.text = listaPacientes[i];
@@ -124,25 +110,29 @@ function getPatients(){
                 select.add(option);
             }
         }
-    } 
+    }
 }
 
-function getMedicines(){
-    //Obtiene el correo electronico y la sesion del sessionStorage
+// Función para obtener la lista de medicinas
+function getMedicines() {
+    // Obtener el correo electrónico y la sesión del sessionStorage
     var mail = sessionStorage.getItem('mail');
     var session = sessionStorage.getItem('session');
-    //envia una solicitud GET al backend con los parametros mail y session
+    
+    // Enviar una solicitud GET al backend con los parámetros mail y session
     var http = new XMLHttpRequest();
-    http.open('GET',"http://localhost:3000/Entornos/ServeMedicines?mail=" + mail + "&session=" +session, true);
+    http.open('GET', "http://localhost:3000/Entornos/ServeMedicines?mail=" + mail + "&session=" + session, true);
     http.send();
-    http.onreadystatechange = function () {
+    
+    http.onreadystatechange = function() {
         console.log("Ready state: " + http.readyState);
         console.log("Status: " + http.status);
         if (http.readyState == 4 && http.status == 200) {
             let listaMedicines = JSON.parse(http.responseText);
-
+            
             let select = document.getElementById("seleccionMedicinas");
-
+            
+            // Recorrer la lista de medicinas y crear opciones para el elemento select
             for (let i = 0; i < listaMedicines.length; i++) {
                 let option = document.createElement("option");
                 option.text = listaMedicines[i].name;
@@ -153,12 +143,15 @@ function getMedicines(){
     }
 }
 
+// Función para enviar un release
 function enviarRelease() {
     let http = new XMLHttpRequest();
 
+    // Obtener el correo electrónico y la sesión del sessionStorage
     let mail = sessionStorage.getItem("mail");
     let session = sessionStorage.getItem("session");
     
+    // Obtener los valores de los elementos del formulario
     let idXip = document.getElementById("idXip").value;
     let mailPaciente = document.getElementById("seleccionPacientes").value;
     let idMed = document.getElementById("seleccionMedicinas").value;
@@ -166,6 +159,8 @@ function enviarRelease() {
 
     http.open("POST", "http://localhost:3000/Entornos/Release", true);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    // Enviar los parámetros en el cuerpo de la solicitud POST
     http.send("mail=" + mail + "&session=" + session + "&xip_id=" + idXip + "&patient_mail=" + mailPaciente + "&id_medicine=" + idMed + "&end_date=" + fecha);
 
     http.onreadystatechange = function () {
@@ -176,5 +171,3 @@ function enviarRelease() {
         }
     }
 }
-
-
